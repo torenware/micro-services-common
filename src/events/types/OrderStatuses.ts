f2defce45e4c21bcaec23ffbd8845e817941cae8
Rob Thorne <rob@torenware.com>
2021-04-28 13:46:23 -0700

    Added OrderStatuses enum

---
+++ src/events/types/OrderStatuses.ts
@@ -0,0 +1,14 @@
+enum OrderStatus {
+  // Initial state
+  Created = 'created',
+
+  // The ticket was cancelled by the user, or it was
+  // already assigned to somebody else, or expired.
+  Cancelled = 'cancelled',
+
+  // Order is awaiting payment.
+  AwaitingPayment = 'awaiting:payment',
+
+  // Paid and given over.
+  Complete = 'complete',
+}
