a33bc1e688b726283153cbb1d14325be076fedbe
Rob Thorne <rob@torenware.com>
2021-04-28 19:37:17 -0700

    Added order events

---
+++ src/events/order-created-event.ts
@@ -0,0 +1,17 @@
+import { Subjects } from './subjects';
+import { OrderStatus } from './types/order-status';
+
+export interface OrderCreatedEvent {
+  subject: Subjects.OrderCreated;
+  data: {
+    id: string;
+    userId: string;
+    status: OrderStatus;
+    // JSON does not know about Dates:
+    expiresAt: string;
+    ticket: {
+      id: string;
+      price: number;
+    };
+  };
+}
