bf66dde9d8016a51ebd01190705378569d78fc3d
Rob Thorne <rob@torenware.com>
2021-05-02 19:10:40 -0700

    Add payment created event

---
+++ src/events/payment-created-event.ts
@@ -0,0 +1,11 @@
+import { Subjects } from './subjects';
+
+export interface PaymentCreatedEvent {
+  subject: Subjects.PaymentCreated;
+  data: {
+    id: string;
+    version: number;
+    orderId: string;
+    stripeId: string;
+  };
+}
